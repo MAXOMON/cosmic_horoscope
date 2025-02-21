8cbcdd158050793cec9698254889602821eb1c14
MRaynold <maxomon@gmail.com>
2025-02-21 18:12:29 +0300

    приложение мигрировало с SQLite3 на PostgreSQL

--- zodiac/settings.py
+++ zodiac/settings.py
@@ -76,10 +76,22 @@ ASGI_APPLICATION = 'zodiac.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+""" OLD VERSION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zodiac',
+        'USER': 'zodiac_user',
+        'PASSWORD': '2310',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
